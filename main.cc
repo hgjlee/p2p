81e8def4c6c1bd0e13f69b080fa10b204ad33714
Jacob Lee <hyunjacoblee@gmail.com>
2017-11-12 15:37:04 -0500

    Initial Commit

---
+++ main.cc
@@ -0,0 +1,142 @@
+
+#include <unistd.h>
+
+#include <QVBoxLayout>
+#include <QApplication>
+#include <QDebug>
+
+#include "main.hh"
+
+ChatDialog::ChatDialog()
+{
+	setWindowTitle("P2Papp");
+
+	// Read-only text box where we display messages from everyone.
+	// This widget expands both horizontally and vertically.
+	textview = new QTextEdit(this);
+	textview->setReadOnly(true);
+
+	// Small text-entry box the user can enter messages.
+	textline = new QLineEdit(this);
+
+	// Lay out the widgets to appear in the main window.
+	QVBoxLayout *layout = new QVBoxLayout();
+	layout->addWidget(textview);
+	layout->addWidget(textline);
+	setLayout(layout);
+
+	socket = new NetSocket();
+	socket->bind();
+
+	//Setting timer
+	timer = new QTimer(this);
+	connect(timer, SIGNAL(timeout()), this, SLOT(timerHandler()));
+
+	//Setting antientropy timer
+	/*
+	anti_timer = new QTimer(this);
+	connect(anti_timer, SIGNAL(timeout()), this, SLOT(antiHandler()));
+	anti_timer->start(10000);
+	*/
+
+	seqnum = 0; 
+	message_status = new QMap<QString, quint32>;
+
+	//Register a callback on the textline's returnPressed signal
+	connect(textline, SIGNAL(returnPressed()), this, SLOT(gotReturnPressed()));
+
+	//Register a callback on the textline's readyRead signal 
+	connect(socket, SIGNAL(readyRead()), this, SLOT(readMessages()));
+}
+
+QByteArray ChatDialog::serialization(QString message){
+	QVariantMap message_qmap;
+
+	//Constructing rumor message
+	message_qmap.insert("ChatText", message);
+	message_qmap.insert("Origin", QString::number(socket->myPort);
+	message_qmap.insert("SeqNo", seqnum);
+	seqnum += 1; 
+
+	//Append new messages to a list
+	if(!message_list.contains(QString::number(socket->myPort))){
+		QMap<quint32, QVariantMap> qmap;
+        message_list.insert(QString::number(mySocket->myPort), qmap);
+        message_list[QString::number(mySocket->myPort)].insert(message_qmap.value("SeqNo").toUInt(), message_qmap);
+	} else {
+		message_list[QString::number(mySocket->myPort)].insert(message_qmap.value("SeqNo").toUInt(), message_qmap);
+	}
+
+	//We serialize the message into QByteArray
+	QByteArray byte_arr;
+	QDataStream stream(&byte_arr,QIODevice::ReadWrite);
+	stream  << message_qmap;
+
+	return byte_arr;
+}
+
+void ChatDialog::gotReturnPressed()
+{
+	// Initially, just echo the string locally.
+	// Insert some networking code here...
+	qDebug() << "Return Pressed. Sending: " << textline->text();
+	textview->append(QString::number(socket->myPort) + ": " + textline->text());
+
+	//Fetch the message then serialize it. 
+	Qstring str_input = textline->text();
+	QByteArray serialized_str = serializeMessage(str_input);
+
+	//Update the neighbors' want list
+	if()
+
+	sendMessages(serialized_str);
+	// Clear the textline to get ready for the next input message.
+	textline->clear();
+}
+
+NetSocket::NetSocket()
+{
+	// Pick a range of four UDP ports to try to allocate by default,
+	// computed based on my Unix user ID.
+	// This makes it trivial for up to four P2Papp instances per user
+	// to find each other on the same host,
+	// barring UDP port conflicts with other applications
+	// (which are quite possible).
+	// We use the range from 32768 to 49151 for this purpose.
+	myPortMin = 32768 + (getuid() % 4096)*4;
+	myPortMax = myPortMin + 3;
+}
+
+bool NetSocket::bind()
+{
+	// Try to bind to each of the range myPortMin..myPortMax in turn.
+	for (int p = myPortMin; p <= myPortMax; p++) {
+		if (QUdpSocket::bind(p)) {
+			qDebug() << "bound to UDP port " << p;
+			return true;
+		}
+	}
+
+	qDebug() << "Oops, no ports in my default range " << myPortMin
+		<< "-" << myPortMax << " available";
+	return false;
+}
+
+int main(int argc, char **argv)
+{
+	// Initialize Qt toolkit
+	QApplication app(argc,argv);
+
+	// Create an initial chat dialog window
+	ChatDialog dialog;
+	dialog.show();
+
+	// Create a UDP network socket
+	NetSocket sock;
+	if (!sock.bind())
+		exit(1);
+
+	// Enter the Qt main loop; everything else is event driven
+	return app.exec();
+}
+
---
+++ main.hh
@@ -0,0 +1,39 @@
+#ifndef P2PAPP_MAIN_HH
+#define P2PAPP_MAIN_HH
+
+#include <QDialog>
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QUdpSocket>
+#include <QTimer>
+
+class ChatDialog : public QDialog
+{
+	Q_OBJECT
+
+public:
+	ChatDialog();
+
+public slots:
+	void gotReturnPressed();
+
+private:
+	QTextEdit *textview;
+	QLineEdit *textline;
+};
+
+class NetSocket : public QUdpSocket
+{
+	Q_OBJECT
+
+public:
+	NetSocket();
+
+	// Bind this socket to a P2Papp-specific default port.
+	bool bind();
+
+private:
+	int myPortMin, myPortMax, myPort;
+};
+
+#endif // P2PAPP_MAIN_HH
